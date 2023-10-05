ef39aa14ae0dd8f3af8d22da87b437e14d72d06e
Riza Semih Koca <rsemihkoca@outlook.com>
2023-10-05 12:00:10 +0300

    commit

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterPage
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'), # next_page ile logout olduktan sonra login sayfasına yönlendiriyoruz.
+    path('register/', RegisterPage.as_view(), name='register'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,29 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView,  DeleteView
+from django.views.generic.edit import CreateView, UpdateView,  DeleteView, FormView
 
-from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 
 from .models import Task
-class TaskList(ListView):
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True # Eğer kullanıcı giriş yapmışsa register sayfasına gitmesini engelliyoruz.
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save() # formdan gelen bilgileri kaydediyoruz.
+        if user is not None:
+            login(self.request, user) # login fonksiyonu ile kullanıcıyı otomatik olarak giriş yapıyoruz.
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated: # Eğer kullanıcı giriş yapmışsa register sayfasına gitmesini engelliyoruz.
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks' # Modelden gelen verileri tasks adında bir değişken ile templateye gönderiyoruz.
-class TaskDetail(DetailView):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) # get_context_data fonksiyonunu override ediyoruz.
+        context['tasks'] = context['tasks'].filter(user=self.request.user) # context['tasks'] değişkenine filtreleme yapıyoruz.
+        context['count'] = context['tasks'].filter(complete=False).count() # context['count'] değişkenine tamamlanmamış görev sayısını gönderiyoruz.
+        search_input = self.request.GET.get('search-area') or '' # search-area adında bir input alanı oluşturuyoruz.
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+        context['search_input'] = search_input # context['search_input'] değişkenine search_input değerini gönderiyoruz.
+        return context
+
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks') # reverse_lazy fonksiyonu ile tasks urline yönlendiriyoruz.
 
-class TaskUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user # form.instance.user ile formdan gelen user bilgisini alıyoruz.
+        return super(TaskCreate, self).form_valid(form) # formu geçerli kılmak için form_valid fonksiyonunu override ediyoruz.
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
