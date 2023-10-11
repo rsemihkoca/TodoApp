1c668fd80e7f9adabfbaa24bf861582995fd1046
rsemihkoca <rsemihkoca@outlook.com>
2023-10-12 01:44:01 +0300

    echo steps

--- build.sh
+++ build.sh
@@ -10,14 +10,17 @@ source todo/bin/activate
 
 # Step 3: Install project dependencies from 'requirements.txt'
 pip install -r requirements.txt
+echo "Dependencies installed"
 
 # Step 4: Navigate to the Django project directory
 # shellcheck disable=SC2164
 cd todo_list
+echo "Directory changed"
 
 # Step 5: Create and apply database migrations
 python manage.py makemigrations
 python manage.py migrate
-
+echo "Database migrated"
 # Step 6: Start the Django development server
 python manage.py runserver
+echo "Server started"
