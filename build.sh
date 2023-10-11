4db345f283147c471af01dd43f858ab411358fee
rsemihkoca <rsemihkoca@outlook.com>
2023-10-12 00:43:50 +0300

    sh file added

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+
+# Step 1: Create the 'todo' virtual environment
+python3 -m venv todo
+
+# Step 2: Activate the 'todo' virtual environment
+source todo/bin/activate
+
+# Step 3: Install project dependencies from 'requirements.txt'
 pip install -r requirements.txt
+
+# Step 4: Navigate to the Django project directory
+# shellcheck disable=SC2164
 cd todo_list
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver
+
+# Step 5: Create and apply database migrations
+python manage.py makemigrations
+python manage.py migrate
+
+# Step 6: Start the Django development server
+python manage.py runserver
